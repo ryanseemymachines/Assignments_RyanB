cf922f1333e3da9d455406aeecac647d2e44d178
Ryan <shameem@seemymachines.com>
2025-10-20 16:49:43 +0530

    Until accordion...mobile view remaining

--- Javascript/assignment3a/script.js
+++ Javascript/assignment3a/script.js
@@ -5,20 +5,36 @@ const servicesClick=document.getElementById("servicesClick");
 
 const addressClick=document.getElementById("addressClick");
 
+
 document.getElementById("content-services").style.display="none";
+
+document.getElementById("content-address").style.display="none";
 
 homeClick.addEventListener("click", displayHome);
 servicesClick.addEventListener("click", displayServices);
+addressClick.addEventListener("click", displayAddress);
 
 function displayHome(){
     document.getElementById("content-home").style.display="flex";
 
     document.getElementById("content-services").style.display="none";
+ 
 }
 
 function displayServices(){
 
     document.getElementById("content-services").style.display="flex";
+
+    document.getElementById("content-home").style.display="none"
+
+    document.getElementById("content-address").style.display="none";
+}
+
+function displayAddress(){
+
+    document.getElementById("content-address").style.display="flex";
+
+    document.getElementById("content-services").style.display="none";
 
     document.getElementById("content-home").style.display="none"
 }
@@ -36,3 +52,31 @@ arrowBtns.forEach(btn => {
     })
 })
 
+const address=document.querySelectorAll(".accordion-content p");
+
+address.forEach(para =>{
+
+    const text=para.textContent;
+
+    const parts=text.split(",");
+
+    para.innerHTML = parts.map(part => part.trim()).join(",<br>");
+});
+
+//accordions
+
+const revealAdd = document.querySelectorAll(".accordion-heading i");
+
+revealAdd.forEach(btn => {
+    btn.addEventListener("click",()=>{
+        const content = btn.parentElement.nextElementSibling;
+
+        if (content.style.display === "flex") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "flex";
+        }
+        
+        btn.classList.toggle("rotate");
+    });
+});
