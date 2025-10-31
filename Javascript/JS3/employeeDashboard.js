78c5852d497114a3fc222250e6386e5787e0511a
Ryan <shameem@seemymachines.com>
2025-10-31 09:17:46 +0530

    started

--- Javascript/JS3/employeeDashboard.js
+++ Javascript/JS3/employeeDashboard.js
@@ -1,14 +1,27 @@
+//Modal elements
+const employeeModal = document.getElementById("employeeModal");
+const modalHeading = document.getElementById("modalHeading");
+const closeModal = document.getElementById("closeModal");
+const employeeForm = document.getElementById("employeeForm");
+const saveEmployee = document.getElementById("saveEmployee");
 
+//Input fields
+const empName = document.getElementById("empName");
+const empDesignation = document.getElementById("empDesignation");
+const empDob = document.getElementById("empDob");
 
-viewEmployee();
+//Modal close function
+closeModal.addEventListener("click",closeModalFunc);
 
-async function viewEmployee(){
-    try{
-        const response= await fetch("https://6580190d6ae0629a3f54561f.mockapi.io/api/v1/employee");
-        const employeeData= await response.json();
-        console.log(employeeData);
-    }
-    catch(error){
-        console.log(error);
-    }
+window.addEventListener("click" ,(e) =>{
+    if(e.target === employeeModal)
+        closeModalFunc(); 
+});
+
+function closeModalFunc(){
+    employeeModal.style.display="none";
+    employeeForm.reset();   
 }
+
+//Base URL
+const API_URL = "https://6580190d6ae0629a3f54561f.mockapi.io/api/v1/employee";
